bf1eaeab424d56b39fd994906d45b49565ef681e
Traipop <traipopwichiansarn@gmail.com>
2026-01-09 16:48:41 +0700

    lab14_2 completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,42 @@ int main(){
 }
 
 //Do not modify source code above this line 
+void showMatrix(const bool A[N][N]){
+	int round = 0;
+	for(int i = 0 ; i < N ; i++){
+		for(int j = 0 ; j < N ; j++){
+			if(round >= N){
+				cout << "\n";
+				round = 0;
+			}
+			cout << A[i][j] << " ";
+			round++;
+		}
+	}
+}
+
+void inputMatrix(double A[N][N]){
+	for(int i = 0 ; i < N ; i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j = 0 ; j < N ; j++){
+			cin >> A[i][j];
+	
+		}
+	}
+}
+
+
+void findLocalMax(const double data[N][N], bool B[N][N]){
+	for(int i = 1 ; i< N-1 ; i++){
+		for(int j = 1 ; j < N-1;j++){
+			if((data[i][j]>=data[i-1][j])&&(data[i][j]>=data[i][j-1])&&(data[i][j]>=data[i][j+1])&&(data[i][j]>=data[i+1][j])){
+				B[i][j] = 1;
+			}
+			else {
+				B[i][j] = 0;
+			}
+    	}
+    }
+}
+
+
